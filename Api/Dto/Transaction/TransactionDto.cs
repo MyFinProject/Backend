a7cabca4f6651f8ea336d23bb26cea259ecce436
xK0NNANx <egoriksolo@gmail.com>
2025-05-13 17:26:20 +0300

    Feat: add Transaction

---
+++ Api/Dto/Transaction/TransactionDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Dto.Transaction
+{
+    public class TransactionDto
+    {
+        [Required]
+        public String UserId { get; set; }
+        [Required]
+        public string WalletId { get; set; } = string.Empty;
+        [Required]
+        public string CategoryId { get; set; }
+        [Required]
+        public int Amount { get; set; }
+        [Required]
+        public string CurrencieId { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
---
+++ Api/Migrations/20250512211802_Fix.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "WalletId",
+                table: "Transactions",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_WalletId",
+                table: "Transactions",
+                column: "WalletId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_UserWallets_WalletId",
+                table: "Transactions",
+                column: "WalletId",
+                principalTable: "UserWallets",
+                principalColumn: "WalletId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_UserWallets_WalletId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_WalletId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "WalletId",
+                table: "Transactions");
+        }
+    }
+}
